13583e382e085bb3da085971630d63f2c920752e
nowlansavage <nowlansavage@gmail.com>
2020-08-16 21:26:01 -0500

    data engineering

---
+++ EmployeeSQL/Schema.sql
@@ -0,0 +1,43 @@
+drop table Employee;
+drop table Title;
+drop table Department_Employee;
+drop table Departments;
+drop table Salary;
+drop table Department_Manager;
+
+create table Employee (
+	EmployeeID int Primary Key,
+	Employee_title varchar,
+	dob varchar,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date varchar
+);
+
+create table Title (
+	titleID varchar Primary Key,
+	title varchar
+);
+
+create table Department_Employee (
+	EmployeeID varchar,
+	departmentID varchar,
+	PRIMARY KEY(EmployeeID, departmentID)
+);
+
+create table Departments (
+	departmentID varchar primary Key,
+	department_name varchar
+);
+
+create table Salary (
+	EmployeeID varchar primary key,
+	salary int
+);
+
+create table Department_Manager (
+	DepartmentID varchar,
+	EmployeeID int primary key
+);
+
